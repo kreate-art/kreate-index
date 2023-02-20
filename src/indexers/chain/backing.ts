77677bcb2e2a4cecc90c651f7ca888ae8b345226
sk-yagi <yagi@shinka.network>
2023-02-21 00:42:05 +0700

    [resync] feat: index backer actions

--- src/indexers/chain/backing.ts
+++ src/indexers/chain/backing.ts
@@ -2,6 +2,7 @@ import * as O from "@cardano-ogmios/schema";
 import { Address } from "lucid-cardano";
 
 import { deconstructAddress } from "@teiki/protocol/helpers/schema";
+import { fromJson, toJson } from "@teiki/protocol/json";
 import * as S from "@teiki/protocol/schema";
 import { BackingDatum } from "@teiki/protocol/schema/teiki/backing";
 import { Hex, UnixTime } from "@teiki/protocol/types";
@@ -25,6 +26,23 @@ export type ChainBacking = {
   unbackedAt: UnixTime | null;
 };
 
+const AllAction = {
+  0: "back",
+  1: "unback",
+  2: "claim_reward", // to be supported
+  3: "migrate", // to be supported
+};
+
+export type ChainBackingAction = {
+  projectId: Hex;
+  actorAddress: Address;
+  amount: Lovelace;
+  message: string | null;
+  time: UnixTime;
+  slot: number;
+  action: keyof typeof AllAction;
+};
+
 export type Event = { type: "backing"; indicies: NonEmpty<number[]> | null };
 const $ = $handlers<TeikiChainIndexContext, Event>();
 
@@ -40,9 +58,7 @@ export const setup = $.setup(async ({ sql }) => {
       backing_amount bigint NOT NULL,
       milestone_backed smallint NOT NULL,
       backing_message text,
-      unbacking_message text,
-      backed_at timestamptz NOT NULL,
-      unbacked_at timestamptz
+      backed_at timestamptz NOT NULL
     )
   `;
   await sql`
@@ -52,6 +68,18 @@ export const setup = $.setup(async ({ sql }) => {
   await sql`
     CREATE INDEX IF NOT EXISTS backing_backer_address_index
       ON chain.backing(backer_address)
+  `;
+  await sql`
+    CREATE TABLE IF NOT EXISTS chain.backing_action (
+      id bigint GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+      project_id varchar(64) NOT NULL,
+      actor_address text NOT NULL,
+      amount bigint NOT NULL,
+      message text,
+      time timestamptz NOT NULL,
+      slot integer NOT NULL,
+      action smallint
+    )
   `;
 });
 
@@ -84,34 +112,59 @@ export const filter = $.filter(
   }
 );
 
+type BackingDiff = {
+  unback: Lovelace;
+  back: Lovelace;
+};
+
+type UnbackResult = {
+  projectId: Hex;
+  totalAmount: Lovelace;
+  actorAddress: Address;
+};
+
+type BackingTupleKey = [Hex, Address];
+
 export const event = $.event(
   async ({
     driver,
     connections: { sql, lucid, slotTimeInterpreter },
-    block: { slot },
+    block: { slot, time },
     tx,
     event: { indicies },
   }) => {
     const txTimeStart = slotTimeInterpreter.slotToAbsoluteTime(
       tx.body.validityInterval.invalidBefore ?? slot
     );
-    const unbackingMessage = extractCip20Message(tx)?.join("\n") || null;
-    // TODO: Bounded unbacked_at?
-    const unbackResult = await sql`
-      UPDATE chain.backing b
-      SET unbacking_message = ${unbackingMessage},
-          unbacked_at = ${txTimeStart}
-      FROM chain.output o
-      WHERE
-        o.id = b.id
-        AND (o.tx_id, o.tx_ix) IN ${sql(
-          tx.body.inputs.map((input) => sql([input.txId, input.index]))
-        )}
+    const message = extractCip20Message(tx)?.join("\n") || null;
+
+    const unbackResult = await sql<UnbackResult[]>`
+      SELECT
+        SUM(b.backing_amount) AS total_amount,
+        b.project_id,
+        b.backer_address AS actor_address
+      FROM chain.backing b
+      INNER JOIN chain.output o ON b.id = o.id
+      WHERE (o.tx_id, o.tx_ix) IN ${sql(
+        tx.body.inputs.map((input) => sql([input.txId, input.index]))
+      )}
+      GROUP BY
+        (b.project_id, b.backer_address)
     `;
+
+    // BackingTupleKey
+    const actionRef: Map<string, BackingDiff> = new Map<string, BackingDiff>();
+    unbackResult.forEach((value) => {
+      const key: BackingTupleKey = [value.projectId, value.actorAddress];
+      actionRef.set(toJson(key), {
+        unback: value.totalAmount,
+        back: 0n,
+      });
+    });
+
     if (indicies != null) {
       // Since backers can unback and back in the same transaction
       // If there's a CIP-20 message, we will count it towards unbackings if possible; otherwise, backings
-      const backingMessage = unbackResult.count ? null : unbackingMessage;
       const backings = await driver.store(indicies, (output) => {
         if (output.datum == null) {
           console.warn(
@@ -121,28 +174,59 @@ export const event = $.event(
           return undefined;
         }
         const backingDatum = S.fromData(S.fromCbor(output.datum), BackingDatum);
+
+        const backerAddress = deconstructAddress(
+          lucid,
+          backingDatum.backerAddress
+        );
+        const projectId = backingDatum.projectId.id;
+        const tupleKey: BackingTupleKey = [projectId, backerAddress];
+        const res = actionRef.get(toJson(tupleKey));
+
+        if (res) {
+          const oldBacking = res.back;
+          const tupleKey: BackingTupleKey = [projectId, backerAddress];
+          actionRef.set(toJson(tupleKey), {
+            unback: res.unback,
+            back: oldBacking + output.value.lovelace,
+          });
+        } else {
+          actionRef.set(toJson(tupleKey), {
+            unback: 0n,
+            back: output.value.lovelace,
+          });
+        }
+
         return [
           "backing",
           {
-            projectId: backingDatum.projectId.id,
-            backerAddress: deconstructAddress(
-              lucid,
-              backingDatum.backerAddress
-            ),
+            projectId,
+            backerAddress,
             backingAmount: output.value.lovelace,
             milestoneBacked: Number(backingDatum.milestoneBacked),
-            backingMessage,
-            // TODO: Remove the `Math.min` part after we do the final migration on testnet
-            // The new contract should already use TxTimeStart instead of TxTimeEnd for backed_at
-            backedAt: Math.min(
-              txTimeStart,
-              Number(backingDatum.backedAt.timestamp)
-            ),
+            backingMessage: message,
+            backedAt: txTimeStart,
           },
         ];
       });
       if (!backings.length) console.warn("there is no valid backing");
       else await sql`INSERT INTO chain.backing ${sql(backings)}`;
+    }
+
+    for (const [_key, value] of actionRef) {
+      const key: BackingTupleKey = fromJson(_key);
+      const delta = BigInt(value.back) - BigInt(value.unback);
+      const action = delta > 0n ? 0 : 1; // back vs unback
+      const backerAction: ChainBackingAction = {
+        projectId: key[0],
+        actorAddress: key[1],
+        amount: delta > 0 ? delta : -delta,
+        message,
+        time, // block time at which executed the action
+        slot,
+        action,
+      };
+      await sql`INSERT INTO chain.backing_action ${sql(backerAction)}`;
     }
     driver.refresh("views.project_summary");
   }
@@ -151,13 +235,8 @@ export const event = $.event(
 export const rollback = $.rollback(
   async ({ connections: { sql, views }, point }) => {
     await sql`
-      UPDATE chain.backing b
-      SET unbacking_message = NULL,
-          unbacked_at = NULL
-      FROM chain.output o
-      WHERE
-        o.id = b.id
-        AND o.spent_slot > ${slotFrom(point)}
+      DELETE FROM chain.backing_action ba
+        WHERE ba.slot > ${slotFrom(point)}
     `;
     views.refresh("views.project_summary");
   }
