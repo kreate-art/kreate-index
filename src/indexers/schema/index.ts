5681a68491588268f941fb6f295c8928b59783bc
Shishi | Shinka <shishi@shinka.network>
2023-03-21 22:08:21 +0700

    feat: schema for kolours

--- src/indexers/schema/index.ts
+++ src/indexers/schema/index.ts
@@ -9,6 +9,7 @@ export const setupSchemas = $setup(async ({ sql }) => {
   await sql`CREATE SCHEMA IF NOT EXISTS ipfs`;
   await sql`CREATE SCHEMA IF NOT EXISTS discord`;
   await sql`CREATE SCHEMA IF NOT EXISTS ai`;
+  await sql`CREATE SCHEMA IF NOT EXISTS kolours`;
   await sql`CREATE SCHEMA IF NOT EXISTS admin`;
   await sql`CREATE SCHEMA IF NOT EXISTS views`;
   // TODO: Remove this `migration` schema later.
